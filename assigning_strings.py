e55e481b54cedf298d5eb130294cf03f46efcb2e
MWANGANGI1999 <stvnmunene@gmail.com>
2020-02-06 09:53:37 +0300

    assigning strings

---
+++ assigning_strings.py
@@ -0,0 +1,16 @@
+#assigning strings in python
+a="Hello Kenya"
+#assigns string starting with the last character
+print(a[-2])
+print(a[-3])
+#the first character is included while the last character is excluded
+print(a[2:5])
+print(a[2:6])
+print(a[2:8])
+print(a[2:7])
+#assigns string starting with the last character
+print(a[-1])
+print(a[-3])
+print(a[3:6])
+
+
